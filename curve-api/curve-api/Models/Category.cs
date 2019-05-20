98e26fe69ab9c6b35b9ecef2d2d1d0d8057bbc7f
Chris <chrismorton.cm@gmail.com>
2019-05-20 14:58:44 -0700

    finished a few of the services

--- curve-api/curve-api/Models/Category.cs
+++ curve-api/curve-api/Models/Category.cs
@@ -7,7 +7,7 @@ namespace curve_api.Models
     public class Category
     {
         //Table
-        public int Name { get; set; }
+        public int Id { get; set; }
         public string CategoryName { get; set; }
         public int OverallScore { get; set; }
         public int ReviewId { get; set; }
