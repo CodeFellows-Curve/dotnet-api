99f0dff74f351f542b7f89eeedb7faee701c7d44
Chris <chrismorton.cm@gmail.com>
2019-05-20 13:26:34 -0700

    removed api database mentions

--- curve-api/curve-api/Models/Services/SubCategoryCommentService.cs
+++ curve-api/curve-api/Models/Services/SubCategoryCommentService.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace curve_api.Database.Models.Services
+namespace curve_api.Models.Services
 {
     class SubCategoryCommentService
     {
