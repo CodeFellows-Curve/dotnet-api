881564926e182a6dea0a94a2fc65c33c83ec480b
iangifford <lightyagamie10@gmail.com>
2019-05-20 13:44:04 -0700

    switch to my branch

---
+++ curve-api/curve-api/Types/ReviewCommentType/ReviewCommentType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraphQL.Types;
+
+namespace curve_api.Types.ReviewCommentType
+{
+    public class ReviewCommentType : ObjectGraphType<Models.ReviewComment>
+    {
+        public ReviewCommentType()
+        {
+            Field(x => x.Id);
+            Field(x => x.UserId);
+            Field(x => x.ReviewId);
+            Field(x => x.Subject);
+            Field(x => x.Content);
+        }        
+    }
+}
---
+++ curve-api/curve-api/Types/SubCategoryCommentType/SubCategoryCommentType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraphQL.Types;
+
+namespace curve_api.Types.SubCategoryCommentType
+{
+    public class SubCategoryCommentType : ObjectGraphType<Models.SubCategoryComment>
+    {
+        public SubCategoryCommentType()
+        {
+            Field(x => x.Id);
+            Field(x => x.UserId);
+            Field(x => x.SubCategoryId);
+            Field(x => x.Subject);
+            Field(x => x.Content);
+        }  
+    }
+}
