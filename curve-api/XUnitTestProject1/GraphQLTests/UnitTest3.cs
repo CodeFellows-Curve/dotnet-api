33e004194d225a0d0e3d75325bd4daecaa729ea0
iangifford <lightyagamie10@gmail.com>
2019-05-21 12:11:04 -0700

    Tests building out. moved folders for individual models to be tested

---
+++ curve-api/XUnitTestProject1/GraphQLTests/UnitTest3.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using curve_api.Models;
+
+namespace XUnitTestProject1.GraphQLTests
+{
+    public class UnitTest3
+    {
+        [Fact]
+        public void Test()
+        {
+
+        }
+    }
+}
