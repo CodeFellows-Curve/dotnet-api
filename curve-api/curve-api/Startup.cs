e4b551292563952efbec75707c1a1d80170e7c14
Chris <chrismorton.cm@gmail.com>
2019-05-21 11:28:55 -0700

    startup changes added back in

--- curve-api/curve-api/Startup.cs
+++ curve-api/curve-api/Startup.cs
@@ -1,18 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using curve_api.Data;
+﻿using curve_api.Data;
+using curve_api.Models.Interfaces;
+using curve_api.Models.Services;
+using curve_api.Queries;
+using curve_api.Schema;
+using GraphQL;
+using GraphQL.Types;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Swashbuckle.AspNetCore.Swagger;
 
 namespace curve_api
@@ -43,8 +42,30 @@ namespace curve_api
 			// Register DB context in services
 			services.AddDbContext<CurveDBContext>(options => options.UseSqlServer(connectionString_CurveDB));
 
-			// Register the Swagger generator, defining 1 or more Swagger documents
-			services.AddSwaggerGen(c =>
+            services.AddDbContext<CurveDBContext>(options =>
+options.UseSqlServer(Configuration["ConnectionStrings:CurveDB"]));
+            services.AddTransient<IIndividualManager, IndividualService>();
+            services.AddTransient<IReviewManager, ReviewService>();
+            services.AddTransient<ICategoryManager, CategoryService>();
+            services.AddTransient<ISubCategoryManager, SubCategoryService>();
+            services.AddTransient<IReviewCommentManager, ReviewCommentService>();
+            services.AddTransient<ICategoryCommentManager, CategoryCommentService>();
+            services.AddTransient<ISubCategoryCommentManager, SubCategoryCommentService>();
+            services.AddSingleton<IDocumentExecuter, DocumentExecuter>();
+            services.AddSingleton<IndividualQuery>();
+            services.AddSingleton<Types.Individual.IndividualType>();
+            services.AddSingleton<Types.Review.ReviewType>();
+            services.AddSingleton<Types.Category.CategoryType>();
+            services.AddSingleton<Types.SubCategory.SubCategoryType>();
+            services.AddSingleton<Types.ReviewCommentType.ReviewCommentType>();
+            services.AddSingleton<Types.Category.CategoryType>();
+            services.AddSingleton<Types.SubCategoryCommentType.SubCategoryCommentType>();
+            services.AddSingleton<Types.Individual.IndividualInputType>();
+            var sp = services.BuildServiceProvider();
+            services.AddSingleton<ISchema>(new CurveSchema(new FuncDependencyResolver(type => sp.GetService(type))));
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
             });
