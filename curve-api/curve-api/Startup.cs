f95b4f47cd019dfa4bec009a7fa5f1e2cfe1632e
Andrew Curtis <curtis.andrewm@gmail.com>
2019-05-22 20:12:25 -0700

    Temp fix for null connection string

--- curve-api/curve-api/Startup.cs
+++ curve-api/curve-api/Startup.cs
@@ -38,13 +38,14 @@ namespace curve_api
             services.AddMvc();
 
 			// Database connection strings
-			//var connectionString_CurveDB = !Environment.IsDevelopment()
-			//									? Configuration["ConnectionStrings:DefaultConnection_CurveDB"] 
-			//									: Configuration["ConnectionStrings:ProductionConnection_CurveDB"];
+			var connectionString_CurveDB = Environment.IsDevelopment()
+												? Configuration["ConnectionStrings:DefaultConnection_CurveDB"]
+												: Configuration["ConnectionStrings:ProductionConnection_CurveDB"];
 
 			// Register DB context in services
+			//services.AddDbContext<CurveDBContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:ProductionConnection_CurveDB"]));
 
-			services.AddDbContext<CurveDBContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:ProductionConnection_CurveDB"]));
+			services.AddDbContext<CurveDBContext>(options => options.UseSqlServer(connectionString_CurveDB));
 
             services.AddTransient<IIndividualManager, IndividualService>();
             services.AddTransient<IReviewManager, ReviewService>();
@@ -71,8 +72,8 @@ namespace curve_api
             var sp = services.BuildServiceProvider();
             services.AddSingleton<ISchema>(new CurveSchema(new FuncDependencyResolver(type => sp.GetService(type))));
 
-            services.AddDbContext<CurveUserDbContext>(options =>
-                options.UseSqlServer(Configuration["ConnectionStrings:CurveUserDb"]));
+            //services.AddDbContext<CurveUserDbContext>(options =>
+            //    options.UseSqlServer(Configuration["ConnectionStrings:CurveUserDb"]));
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<CurveUserDbContext>()
